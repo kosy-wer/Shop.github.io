a0ac521763d4a961171264be656498e23e63cc19
kosy-wer <tankerjohnsonmustang@gmail.com>
2024-02-11 11:12:14 +0700

    1.3.3

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -1,30 +1,38 @@
-<?php 
+<?php
 
-// app/Http/Controllers/WishlistController.php
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Wishlist;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
 class WishlistController extends Controller
 {
-    public function addToWishlist($name_product )
+    public function addToWishlist($product_id)
     {
-    
+        // Mendapatkan user yang terotentikasi
+        $user_id = Auth::ID();
 
-        $user_id = Auth::id();
+        // Pemeriksaan apakah kombinasi product_id dan user_id sudah ada
+        $existingWishlistItem = Wishlist::where('user_id', $user_id)
+            ->where('product_id', $product_id)
+            ->first();
 
-        // Menyimpan data ke dalam Wishlist
+        if ($existingWishlistItem) {
+            // Jika sudah ada, beri respon sesuai kebutuhan Anda
+            return response()->json(['message' => 'Product already in wishlist']);
+        }
+
+        // Jika belum ada, simpan ke dalam Wishlist dengan mencantumkan nilai 'created_at'
         Wishlist::create([
             'user_id' => $user_id,
-            'product_id' => $name_product, // Disesuaikan sesuai kebutuhan Anda
-	    'created_at' => Carbon::now(), // Menggunakan Carbon untuk mendapatkan waktu saat ini
-	]);
+            'product_id' => $product_id,
+            'created_at' => Carbon::now(),
+            // tambahkan kolom lainnya sesuai kebutuhan
+        ]);
 
         return response()->json(['message' => 'Product added to wishlist successfully']);
-
-    
     }
 }
 
