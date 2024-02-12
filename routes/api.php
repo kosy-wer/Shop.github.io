87c58c3a3e7b934a8ab84c5dc71399be1de049f6
kosy-wer <tankerjohnsonmustang@gmail.com>
2024-02-12 19:06:35 +0700

    1.3.6

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WishlistController;
 use Twilio\Rest\Client;
-
+use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,13 +25,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
 $sid    = "ACd4fc4b9fbf52cd8223c9d32957770f11";
 $token  = "d7d593080632eefd589dc6f941a64508";
 $twilio = new Client($sid, $token);
+$product = Product::where('Product_ID', $msg)->first();
+$product_name = $product->Product_Name;
 
 $message = $twilio->messages
     ->create("whatsapp:+6285648403583", // to
-        [
-            "from" => "whatsapp:+14155238886",
-            "body" => $msg
-        ]
+
+    	[
+    "from" => "whatsapp:+14155238886",
+    "body" => "Nama Produk: $product_name\nJumlah Barang: $msg"
+]
+
+
+    
     );
 
 print($message->sid);
